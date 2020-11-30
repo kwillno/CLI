536481244de3a8ca77b8e782cec026b5a35702ce
Kristian Willassen <kristian@kwill.no>
2020-11-30 09:47:05 +0100

    Added empirical functions to prob

--- math/prob.py
+++ math/prob.py
@@ -33,6 +33,20 @@ def poissonFunc(x,l):
 	p = (((l)**(x))/(factorial(x)))*((np.e)**(-l))
 	return p
 
+def avg(list):
+	arr = np.array(list)
+	sm = arr.sum()
+	avrg = sm / (len(arr))
+	return avrg
+
+def S(list):
+	mu = avg(list)
+	arr = np.array(list)
+	n = len(arr)
+	sm = np.sum((arr-mu)**2)
+	s = np.sqrt((1/(n-1))*sm)
+	return mu, s, n
+
 
 @click.command()
 
@@ -45,6 +59,7 @@ def poissonFunc(x,l):
 @click.option("--choice","-x","x",type=int, help="Choice of x in hypergeometric and binomial models.")
 @click.option("--probability","-p","p",type=float, help="Probability used in binomial model.")
 @click.option("--lambda","-l","l",type=float, help="Lambda for poisson distribution.")
+@click.option("--list","-li","li",type=str, help="Lambda for poisson distribution.")
 
 
 # Input of commandflags
@@ -55,9 +70,11 @@ def poissonFunc(x,l):
 @click.option("--negativebinomial","-nb","negbin",count=True,default=False,help="Negative binomial distribution, required arguments -x -k -p")
 @click.option("--hypergeometric","-hg","hypGeo",count=True,default=False,help="Hypergeometric distribution, required arguments -x -N -n -k")
 @click.option("--poisson","-pois","poisson",count=True,default=False,help="Poisson distribution, required arguments -x -l")
+@click.option("--average","-mu","mu",count=True,default=False,help="Returns average of listed elemetns, required arguments -li")
+@click.option("--deviation","-s","s",count=True,default=False,help="Returns empirical standard deviation, required arguments -li")
 
 
-def process(N,n,k,r,x,p,l,nCr,nPr,binomial,negbin,hypGeo,poisson):
+def process(N,n,k,r,x,p,l,li,nCr,nPr,binomial,negbin,hypGeo,poisson,mu,s):
 	"""
 	Calculates probabilities of distrubutions given parameters.
 	Can also calculate nPr and nCr
@@ -74,6 +91,14 @@ def process(N,n,k,r,x,p,l,nCr,nPr,binomial,negbin,hypGeo,poisson):
 		print(hyperGeometricFunc(x,N,n,k))
 	if poisson:
 		print(poissonFunc(x,l))
+	if mu:
+		li = eval(li)
+		print(avg(li))
+	if s:
+		li = eval(li)
+		mu, s, n = S(li)
+		print("Average (mu): {:.4f} \nStandard deviation (s): {:.4f} \nSamples (n): {:.0f}".format(mu, s, n))
+
 
 
 if __name__=="__main__":
